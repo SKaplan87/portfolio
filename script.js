cfb45470347c1f8acf46e92a399bc8d6b2d53b44
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-06-04 19:56:07 -0400

    added portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//I need to close all tabs before the rest. We want only one tab to be open at
+//I want to close any open tabs when you click a different tab
 
 $("#about").click(function () {
     $header = $(this);
@@ -96,4 +96,20 @@ $("#conclusion").click(function () {
     });
 
 });
+$("#portfolio").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            // return $content.is(":visible") ? "About -- And Basic CSS" : "About -- And Basic CSS";
+        });
+    });
+});
+
+
 //credit to http://jsfiddle.net/hungerpain/eK8X5/7/
