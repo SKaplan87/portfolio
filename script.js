be6d25706f3dd767432cae7338615f04ea4c6ca4
Scott Kaplan <scottkaplan.sk@gmail.com>
2017-06-04 20:36:38 -0400

    keep adding

--- script.js
+++ script.js
@@ -110,6 +110,19 @@ $("#portfolio").click(function () {
         });
     });
 });
-
+$("#skills").click(function () {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of header based on visibility of content div
+        $header.text(function () {
+            //change text based on condition
+            // return $content.is(":visible") ? "About -- And Basic CSS" : "About -- And Basic CSS";
+        });
+    });
+});
 
 //credit to http://jsfiddle.net/hungerpain/eK8X5/7/
